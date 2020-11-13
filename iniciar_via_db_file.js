a7fb4bce32f7c73eab6a393509055a0cd8f033c0
Mauricio N. Ferreira <contato@ferreiramauricio.com>
2020-11-13 18:48:14 -0300

    Adicionado SELECT para calculo de casos por sexo

--- iniciar_via_db_file.js
+++ iniciar_via_db_file.js
@@ -25,6 +25,23 @@ app.get('/total-casos', (req, res) => {
     
 })
 
+app.get('/total-por-sexo', (req, res) => {
+    db.get(`SELECT total_feminino.total as total_feminino, total_masculino.total as total_masculino FROM
+            (SELECT count(*) as total FROM casos WHERE sexo = 'FEMININO') as total_feminino
+            JOIN 
+                (SELECT count(*) as total FROM casos WHERE sexo = 'MASCULINO') as total_masculino
+            ON (1 = 1)`, (err, row) => {
+        if (err) console.log("Erro ao buscar o total de casos por sexo: " + err)
+
+        if (row) {
+            res.send(row)
+        } else {
+            res.send("Problema ao consultar os dados por sexo.")
+        }
+    })
+    
+})
+
 app.listen(port, () => {
     console.log(`API v1 COVID-IFC rodando em http://localhost:${port}`)
 })
